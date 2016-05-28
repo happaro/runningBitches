5ee46af524314581a4e70d1c900f4597f4b6cb62
Happaro <happaro@gmail.com>
2016-05-28 21:06:46 +0300

    Jump, jump input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,24 +7,36 @@ public class PlayerController : MonoBehaviour
 	public float speed = 10;
 	public float rotateSpeed = 5;
 	public float jumpForce = 200;
-	public bool fourDir;
 	public int currentPlayerNum = 1;
 
 	private float h = 0, v = 0;
+	private bool grounded;
 
-	void Update()
+	void OnCollisionEnter(Collision coll)
 	{
-		if (Input.GetButtonDown ("Cancel"))
-			SceneManager.LoadScene(0);
+		grounded = coll.gameObject.tag == "Ground" ? true : grounded;
+	}
+
+	void OnCollisionExit(Collision coll)
+	{
+		grounded = coll.gameObject.tag == "Ground" ? false : grounded;
+	}
+
+	void Start()
+	{
+		foreach (var joy in Input.GetJoystickNames())
+			Debug.LogWarning(joy);
+		
+	}
+		
+	void Update()
+	{	
 		Movement();
 		Rotating();
-
-		/*if (Mathf.Abs(Input.GetAxis("joy1_horizontal")) > 0.1f)
-			Debug.LogWarning("Joy 1 : " + Input.GetAxis("joy1_horizontal"));
-		if (Mathf.Abs(Input.GetAxis("joy2_horizontal")) > 0.1f)
-			Debug.LogWarning("Joy 2 : " + Input.GetAxis("joy2_horizontal"));*/
-		if (Input.GetButtonDown ("Jump") && this.transform.position.y < 26)
+		if (Input.GetButtonDown (string.Format("joy{0}_button0", currentPlayerNum)) && grounded)
 			Jump();
+		if (Input.GetButtonDown ("best"))
+			SceneManager.LoadScene(0);
 	}
 
 	void Movement()
@@ -40,23 +52,6 @@ public class PlayerController : MonoBehaviour
 
 	void Rotating()
 	{
-		if (fourDir)
-		{
-			if (!(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0))
-			{
-				h = Input.GetAxis("Horizontal");
-				v = Input.GetAxis("Vertical");
-			}
-			var targetObjPos = new Vector3(this.transform.position.x + h, this.transform.position.y, this.transform.position.z + v);
-			var targetRotation = Quaternion.LookRotation(targetObjPos - transform.position);
-			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
-		}
-		else
-		{
-			transform.Rotate(0, rotateSpeed * Input.GetAxis(string.Format("joy{0}_horizontal", currentPlayerNum)), 0);
-
-			//var newRotation = new Quaternion(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + 10 * Input.GetAxis("Horizontal"), transform.rotation.eulerAngles.z, 0);
-			//transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, speed * Time.deltaTime);
-		}
+		transform.Rotate(0, rotateSpeed * Input.GetAxis(string.Format("joy{0}_horizontal", currentPlayerNum)), 0);
 	}
 }
